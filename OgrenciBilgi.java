f204d01c0efb59218e6e73c736befbd81236e14a
250541022 <250541022@firat.edu.tr>
2025-11-06 00:13:13 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: Elif Babürhan
+ * Ogrenci No: 250541022
+ * Tarih: 05/11/2025
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
@@ -10,51 +10,41 @@
  */
 
 import java.util.Scanner;
+public class Ogrencibilgisistemi {
+    public static void main(String[] args)
+        Scanner input = new Scanner(System.in);
 
-public class OgrenciBilgi {
-    public static void main(String[] args) {
-        // Scanner objesi olusturun
-        
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
-        System.out.println("=== OGRENCI BILGI SISTEMI ===");
-        System.out.println();
-        
-        // Ad
-        
-        
-        // Soyad
-        
-        
-        // Ogrenci No
-        
-        
-        // Yas
-        
-        
-        // GPA
-        
-        
-        // Bilgileri ekrana yazdirin
-        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
-        
-        // COZUMUNUZU BURAYA YAZIN
-        
-        
-        
-        
-        
-        
+    System.out.println("===Ogrencibilgisistemi===");
 
-        
-        
-        // Scanner'i kapatin (önemli pratik)
-        
+    System.out.println("Adinizi giriniz:");
+    String adi = input.nextLine();
+
+    System.out.println("Soyadınızı giriniz:");
+    String soyad = input.nextLine();
+
+    System.out.println("Oğrenci numaranızı giriniz:");
+    int numara = input.nextLine();
+
+    System.out.println("Yasınızı giriniz:");
+    int yas = input.nextInt();
+
+    System.out.println("GPA notunuzu giriniz:");
+    double GPA = input.nextDouble();
+
+    // GPA notuna göre başarı durumu kontrol edilir
+
+    String BasariDurumu;
+    if(gpa > = 2) {
+        BasariDurumu = "Basarili Ogrenci";
     }
+    else if(gpa < = 2) {
+        BasariDurumu = "Basarisiz Ogrenci";
+    }
+    System.out.println("===Ogrencibilgisistemi===");
+    System.out.println("Ad Soyad:" adi + soyad);
+    System.out.println("Ogrenci numarası:" + numara );
+    System.out.println("Yas: " + yas );
+    System.out.println("GPA : %.2f%n", GPA );
+
+    input.close();
 }
