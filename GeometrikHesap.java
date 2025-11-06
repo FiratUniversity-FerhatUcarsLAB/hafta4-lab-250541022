a2c642e1e7f629873555875d31b82d335b3b3105
250541022 <250541022@firat.edu.tr>
2025-11-06 23:18:22 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,3 +1,10 @@
+/*
+ * Ad Soyad: Elif Babürhan
+ * Ogrenci No: 250541022
+ * Tarih: 05/11/2025
+ * Aciklama: Gorev 2 - Geometrik Hesap
+    
+     
 import  java.util.Scanner;
 public class GeometrikHesap {
     public static void main(String[] args){
