2a75bdd678c0e3cbc791ddc17d319c0fed434f2a
250541022 <250541022@firat.edu.tr>
2025-11-06 23:15:57 +0300

    Update MaasHesap.java

--- MaasHesap.java
+++ MaasHesap.java
@@ -2,7 +2,7 @@
  * Ad Soyad: Elif Babürhan
  * Ogrenci No: 250541022
  * Tarih: 05/11/2025
- * Aciklama: Gorev 2 - Maas Hesap
+ * Aciklama: Gorev 3 - Maas Hesap
  * Bir çalışanın maaş bilgilerini hesaplayan detaylı bir program yazılır
  * 
  */
